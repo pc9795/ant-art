520626dcd778856a76495a671aa03a1bbed1df7f
Prashant <prashantchaubey9795@gmail.com>
2020-05-01 03:13:48 +0100

    Updated documentation on configuration

--- Readme.md
+++ Readme.md
@@ -1,4 +1,35 @@
 ## Ant Art
 
-* `Main` class is the entry point of the project
-* Thanks [Palette list](https://lospec.com/palette-list) for there free awesome palettes. 
+Import this project as a maven project in your prefered IDE
+* Intellij - https://www.lagomframework.com/documentation/1.6.x/java/IntellijMaven.html
+* Eclipse - https://vaadin.com/learn/tutorials/import-maven-project-eclipse
+
+### Important directories
+* THESE DIRECTORIES ARE ACCEPTED TO BE AT THE ROOT DIRECTORY OF THE PROJECT.
+* `inputs` - The default directory from where the code will pick input images
+* `outputs` - The default directory where the code will dump the outputs
+* `pallets` - The default directory where code looks for colour pallets to paint the image
+* `processed` - The default directory where code transfers an image after processing.
+
+### Main configurations
+
+All the configurations are present in the class `config.Configuration`. Most of the configurations are set over a course 
+of period through experimentation. Some configurations which shouldn't be changed without experimentation are clearly 
+marked. The ones which can be changed to play with the outputs are mentioned below.
+
+Below configurations can be used by the grader to experiment with the system.
+* `Configuration.GUI.FPS` - Change this setting to increase/decrease the simulation speed but be mindful that if we 
+increase the simulation speed we can't able to process large input files. Adjust `Configuarion.MAXIMUM_IMAGE_SIZE` accordingly.
+* `Configuration.GUI.DURATION` - Increase/decrease the length of simulation
+* `Configuration.MAX_ANTS` - The number of ants to spawn in the system.
+* `Configuration.PHEROMNONE_DECAY_RATE` - The speed at which home and food pheromone levels are decayed.
+* `Configuration.MAXIMUM_IMAGE_SIZE` - The maximum size the system can handle. Larger images are scaled down. This will
+in sync with `Configuration.GUI.FPS`. Larger images require less FPS because of processing overhead.
+* `Configuration.DEFAULT_TARGET_COOLOR_COUNT` - The no of colors ants will look for. This setting **highly affects the output**.
+ 
+### How to run the project
+
+
+### Acknowledgements
+* Thanks [Lopsec](https://lospec.com/palette-list) for there free awesome palettes.
+* Thanks Amy Dyer for excellent [tutorial](http://amydyer.art/wp/index.php/2020/01/01/drawing-with-ants-generative-art-with-ant-colony-optimization-algorithms/) on ant colony optimization.
--- src/main/java/ant_art/config/Configuration.java
+++ src/main/java/ant_art/config/Configuration.java
@@ -18,6 +18,10 @@ public final class Configuration {
         private OilPainting() {
         }
 
+        //**************
+        //DO NOT CHANGE
+        //**************
+        //Change the oil painting effect applied to the output images.
         public static final int RADIUS = 5;
         public static final int INTENSITY_LEVEL = 24;
     }
@@ -30,14 +34,24 @@ public final class Configuration {
         }
 
         //DON'T USE SLASHES AT THE END OF THESE DIRECTORIES. THE CODE ASSUMES THAT THESE WILL NOT HAVE SLASHES AT THE END.
+        //Directory to look for color pallets
         public static final String PALLETS = "pallets";
+        //Directory to look for inputs
         public static final String INPUT = "inputs";
+        //Directory to look for outputs
         public static final String OUTPUT = "outputs";
+        //Directory to move the images after processing
         public static final String PROCESSED = "processed";
 
+        //**************
+        //DO NOT CHANGE
+        //**************
         //THESE SET OF DIRECTORIES ARE ASSUMED TO BE RELATIVE TO OUTPUT DIRECTORY
+        //Directory where gifs are moved inside output directory
         public static final String GIF_RELATIVE = "gifs";
+        //Directory where final image is generated inside output directory
         public static final String RAW_RELATIVE = "raw";
+        //Directory where oil painted image is generated inside output directory
         public static final String OIL_PAINTED_RELATIVE = "oil_painted";
     }
 
@@ -52,8 +66,13 @@ public final class Configuration {
         public static final int FPS = 45;
         //Time period of the simulation in seconds
         public static final int DURATION = 30;
+
+        //**************
+        //DO NOT CHANGE
+        //**************
         //Interval at which have to take a image snapshot
         public static final int SAMPLE_INTERVAL = 5;
+        //This parameter keeps check the cases when simulation is not able to run properly.
         public static final int UPDATE_ACCUMULATION_THRESHOLD = 5;
     }
 
@@ -64,22 +83,41 @@ public final class Configuration {
         private Outputs() {
         }
 
+        //**************
+        //DO NOT CHANGE
+        //**************
+        //Image format used in the project
         public static final String IMG_FORMAT = "jpg";
     }
 
     /**
      * Configuration for colors in the ant area.
-     * todo find why needed static
      */
     public static final class Colors {
         private Colors() {
         }
 
-        public static final Color DEFAULT = Color.black;
+        //**************
+        //DO NOT CHANGE
+        //**************
+        //Names are self-explanotry
+        public static final Color DEFAULT = Color.BLACK;
         public static final Color FOOD = Color.RED;
         public static final Color NEST = Color.GREEN;
     }
 
+    //No of ants to generate
+    public static final int MAX_ANTS = 100;
+    //Decay rate of pheromone.
+    public static final float PHEROMONE_DECAY_RATE = 0.005f;
+    //520 works best with an FPS of 60 so change accordingly. If increasing the FPS reduce the size and vice-versa.
+    public static final int MAXIMUM_IMAGE_SIZE = 520;
+    //This is the main setting which will greatly affect the output images. The number of colors to look for in images.
+    public static final int DEFAULT_TARGET_COLOR_COUNT = 2;
+
+    //**************
+    //DO NOT CHANGE
+    //**************
     //This parameter decides the probability of ant actions. A value of 5 means that out of 5 chances ant will do a
     //thing. A probability of 1/5 = 0.2.
     public static final int ANT_SELECTION_SEED = 5;
@@ -94,31 +132,34 @@ public final class Configuration {
     // The color of the cell depends upon the food pheromone present on it. This multiplier will give push for low
     //pheromone levels
     public static final float INTENSITY_AMPLIFIER = 25f;
+    //The number of cells the nest occupy
     public static final int NEST_AREA_SIZE = 5;
+    //The number of cells food occupy - NOT USED RIGHT NOW
     public static final int FOOD_AREA_SIZE = 1;
+    //The side of a cell in pixel length
     public static final int CELL_SIZE = 4;
-    public static final int MAX_ANTS = 50;
+    //Food source to spawn in the image - NOT USED RIGHT NOW
     public static final int FOOD_CELLS_TO_SPAWN = 20;
+    //Nests to spawn in the image
     public static final int NEST_CELLS_TO_SPAWN = 1;
+    //Default amount of food in the cell
     public static final int DEFAULT_FOOD_IN_CELL = 5;
-    public static final float PHEROMONE_DECAY_RATE = 0.005f;
+    //Minimum pheromone level to be in a cell. If below then the colour of the cell is set to default
     public static final float MINIMUM_PHEROMONE_THRESHOLD = 0.01f;
+    //Gain of pheromone when an ant in a cell
     public static final float PHEROMONE_GAIN = 1;
+    //Food capacity of the ants.
     public static final int ANT_FOOD_CAPACITY = 100;
-
-    //Color related Configuration
-    //todo move to separate class
-
+    //The range to look for when checking two colors for similarity
     public static final int COLOR_SIMILARITY_THRESHOLD = 50;
-    //520 works best with an FPS of 60 so change accordingly. If increasing the FPS reduce the size and vice-versa.
-    public static final int MAXIMUM_IMAGE_SIZE = 650;
+    //Number of times rescaling is applied.
     public static final int MAXIMUM_RESCALING_DEPTH = 3;
+    //The percentage above which colors are removed.
     public static final float COLOR_FILTERING_HIGHER_LIMIT = 0.5f;
+    //The percentage below which colors are removed
     public static final float COLOR_FILTERING_LOWER_LIMIT = 0.05f;
-
+    //Delay between images in the output GIF
     public static final int OUTPUT_GIF_DELAY = 250;
+    //Whether the GIF will be looping or not.
     public static final boolean OUTPUT_GIF_LOOPING = true;
-
-    //This is the main setting which will greatly affect the output images. The number of colors to look for in images.
-    public static final int DEFAULT_TARGET_COLOR_COUNT = 2;
 }
